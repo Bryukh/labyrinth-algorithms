30e5609106051f05a842201c60414aab07023462
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-21 22:55:21 +0300

    Add Example maze graph converting

---
+++ js/labyrinth.js
@@ -0,0 +1,97 @@
+var CELL = 30;
+var STEP_TIME = 200;
+
+function drawMaze(mazeBase, svg) {
+    var height = mazeBase.length,
+        width = mazeBase[0].length;
+    var s = Snap(svg);
+    var mazeElements = [];
+    s.attr({"width": CELL * width, "height": CELL * height});
+    for (var h = 0; h < height; h++) {
+        var row = [];
+        for (var w = 0; w < width; w++) {
+            var r = s.rect(w * CELL, h * CELL, CELL, CELL);
+            r.addClass("cell");
+            r.addClass(mazeBase[h][w] == 1 ? "wall" : "empty");
+            row.push(r);
+        }
+        mazeElements.push(row);
+    }
+    return mazeElements;
+}
+
+function recolorMaze(mazeEl) {
+    var height = mazeEl.length,
+        width = mazeEl[0].length;
+    for (var h = 0; h < height; h++) {
+        for (var w = 0; w < width; w++) {
+            var r = mazeEl[h][w];
+            setTimeout((function (el) {
+                return function () {
+                    el.addClass("recolor");
+                }
+            }(r)), (h * width + w) * STEP_TIME);
+        }
+    }
+}
+
+function createGraph(mazeBase, svg) {
+    var height = mazeBase.length,
+        width = mazeBase[0].length;
+    var s = Snap(svg);
+
+    s.attr({"width": CELL * width, "height": CELL * height});
+    for (var h = 0; h < height; h++) {
+        for (var w = 0; w < width; w++) {
+            if (mazeBase[h][w] === 0) {
+                setTimeout((function (j, k) {
+                    return function () {
+                        if (mazeBase[j + 1] && mazeBase[j + 1][k] === 0) {
+                            console.log(111);
+                            var p = s.path(Snap.format("M{x},{y}L{x},{y}",
+                                {
+                                    x: (k + 0.5) * CELL,
+                                    y: (j + 0.5) * CELL
+                                }));
+                            p.addClass("graph-edge");
+                            p.animate(
+                                {
+                                    "path": Snap.format("M{x},{y1}L{x},{y2}",
+                                        {
+                                            x: (k + 0.5) * CELL,
+                                            y1: (j + 0.5) * CELL,
+                                            y2: (j + 1.5) * CELL
+                                        })
+                                }, STEP_TIME);
+                            var c = s.circle((k + 0.5) * CELL, (j + 1.5) * CELL, 1).addClass("cell graph-node");
+                            c.animate({"r": CELL / 3}, STEP_TIME);
+                        }
+                        if (mazeBase[j][k + 1] === 0) {
+                            console.log(111);
+                            p = s.path(Snap.format("M{x},{y}L{x},{y}",
+                                {
+                                    x: (k + 0.5) * CELL,
+                                    y: (j + 0.5) * CELL
+                                }));
+                            p.addClass("graph-edge");
+                            p.animate(
+                                {
+                                    "path": Snap.format("M{x1},{y}L{x2},{y}",
+                                        {
+                                            x1: (k + 0.5) * CELL,
+                                            x2: (k + 1.5) * CELL,
+                                            y: (j + 0.5) * CELL
+                                        })
+                                }, STEP_TIME);
+                            c = s.circle((k + 1.5) * CELL, (j + 0.5) * CELL, 1).addClass("cell graph-node");
+                            c.animate({"r": CELL / 3}, STEP_TIME);
+                        }
+
+                        c = s.circle((k + 0.5) * CELL, (j + 0.5) * CELL, 1).addClass("cell graph-node");
+                        c.animate({"r": CELL / 3}, STEP_TIME);
+                    }
+                })(h, w), (h * width + w) * STEP_TIME);
+            }
+        }
+    }
+}
